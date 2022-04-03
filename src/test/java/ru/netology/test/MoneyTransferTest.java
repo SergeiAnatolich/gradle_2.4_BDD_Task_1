f0d86e5157b3b4b81c487531718254dc03dc5134
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-04-03 16:31:41 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,21 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+}
+
